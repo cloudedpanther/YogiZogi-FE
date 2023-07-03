64bb3bb3d2391986ee433951071c52b5ba00b8ab
Kim Eunjeong <ryomi.j78@gmail.com>
2023-07-03 10:57:30 +0900

    :memo:  chore: make Calendar component from SearchBar - separate it

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,12 +1,9 @@
 import { useEffect, useRef, useState } from 'react';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-import { ko } from 'date-fns/esm/locale';
 import { GetGeoInfo } from '../../utils/getGeoInfo';
 import { getDateFormat, getMonthDayFormat } from '../../utils/handleDate';
-import './SearchBar.css';
 import { useNavigate } from 'react-router-dom';
 import { AlertModal } from '../../components/common/AlertModal';
+import { Calendar } from './Calendar';
 
 export interface SearchProps {
   searchValue: string;
@@ -143,59 +140,7 @@ export const SearchBar = () => {
               ? dateContent
               : '날짜 선택하기'}
           </label>
-          <input type="checkbox" id="calendar" className="modal-toggle" />
-          <div className="modal">
-            <div className="modal-box shadow-lg">
-              <div className="flex justify-center gap-3">
-                <div className="flex flex-col gap-2">
-                  <span className="font-semibold">체크인</span>
-                  <DatePicker
-                    locale={ko}
-                    inline
-                    minDate={new Date(2023, 6, 1)}
-                    maxDate={new Date(2023, 8, 30)}
-                    selected={search.checkInDate}
-                    closeOnScroll={true}
-                    onChange={(date: Date) => {
-                      if (search.checkOutDate)
-                        handleSearchState('checkOutDate', new Date());
-                      handleSearchState('checkInDate', date);
-                    }}
-                  />
-                </div>
-                <div className="flex flex-col gap-2">
-                  <span className="font-semibold">체크아웃</span>
-                  <DatePicker
-                    locale={ko}
-                    inline
-                    minDate={new Date(search.checkInDate.getTime() + 86400000)}
-                    maxDate={new Date(2023, 9, 1)}
-                    selected={search.checkOutDate}
-                    closeOnScroll={true}
-                    onChange={(date: Date) => {
-                      if (
-                        !search.checkInDate ||
-                        search.checkInDate.getTime() >= date.getTime()
-                      )
-                        return;
-                      handleSearchState('checkOutDate', date);
-                    }}
-                  />
-                </div>
-              </div>
-              <div className="modal-action">
-                <label
-                  htmlFor="calendar"
-                  className="btn btn-sm bg-red-500 hover:bg-red-600 text-white"
-                >
-                  확인
-                </label>
-              </div>
-            </div>
-            <label className="modal-backdrop" htmlFor="calendar">
-              Close
-            </label>
-          </div>
+          <Calendar search={search} handleSearchState={handleSearchState} />
         </div>
         <div className="flex flex-col gap-2 sm:w-1/4">
           <div className="flex items-center font-semibold gap-1 text-lg">
