fe5fef949eb21a1050fe25b0af56a8cbfeceab9d
Kim Eunjeong <ryomi.j78@gmail.com>
2023-07-03 17:46:53 +0900

    :sparkles:  feat: add function to close comparison box when click outside

--- src/components/floatingIcons/FloatingIcon.tsx
+++ src/components/floatingIcons/FloatingIcon.tsx
@@ -1,12 +1,13 @@
 import { MdOutlineBedroomParent } from 'react-icons/md';
 import { BsBuilding } from 'react-icons/bs';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { ComparisonBox } from './comparison/ComparisonBox';
 import { useRecoilValue } from 'recoil';
 import {
   selectedAccommodation,
   selectedRoom
 } from '../../store/atom/comparisonAtom';
+import { useClickOutside } from '../../hooks/useClickOutside';
 
 export const FloatingIcon = () => {
   const selectedAcc = useRecoilValue(selectedAccommodation);
@@ -14,8 +15,15 @@ export const FloatingIcon = () => {
   const [comparisonState, setComparisonState] = useState(false);
   const [source, setSource] = useState('');
 
+  const floatingIconsRef = useRef<HTMLDivElement>(null)
+
+  const handleOutsideClick = () => {
+    setComparisonState(false)
+  }
+
+  useClickOutside(floatingIconsRef, handleOutsideClick)
   return (
-    <div className="fixed bottom-[5%] flex flex-col gap-1 md:gap-5 right-1 sm:right-5 md:right-10 z-20">
+    <div ref={floatingIconsRef} className="fixed bottom-[5%] flex flex-col gap-1 md:gap-5 right-1 sm:right-5 md:right-10 z-20">
       <div className="indicator">
         <span className="indicator-item badge bg-red-500 text-white w-4 h-4 md:w-6 md:h-6 right-2 top-3 text-[10px] md:text-base border-none">
           {selectedRooms.length}
