a7beaab30ae22f5c31cd41a3011fb8f83f4d34b8
Kim Eunjeong <ryomi.j78@gmail.com>
2023-07-05 14:32:45 +0900

    :memo:  chore: delete unnecessary code and add date select logic

--- src/components/searchBar/Calendar.tsx
+++ src/components/searchBar/Calendar.tsx
@@ -1,77 +1,64 @@
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { ko } from 'date-fns/esm/locale';
-import { SearchProps } from './SearchBar';
-import './Calendar.css';
 import { useState } from 'react';
+import './Calendar.css';
 
-export const Calendar = ({
-  handleSearchState
-}: {
-  handleSearchState: Function;
-}) => {
-  const [startDate, setStartDate] = useState<Date | null>(new Date());
-  const [endDate, setEndDate] = useState<Date | null>(null);
-
-  const onChange = (dates: [Date | null, Date | null] | null) => {
-    if (dates) {
-      const [start, end] = dates;
-      handleSearchState('checkInDate', start);
-      handleSearchState('checkOutDate', end);
-      setStartDate(start);
-      setEndDate(end);
-    }
-  };
 interface ICalendar {
-  search: SearchProps;
   handleSearchState: Function;
   calendarState: boolean;
   setCalendarState: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const Calendar = ({
-  search,
   handleSearchState,
   calendarState,
   setCalendarState
 }: ICalendar) => {
-  const calendarRef = useRef<HTMLDivElement>(null);
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
+  const [endDate, setEndDate] = useState<Date | null>(null);
+
+  const onCalendarChange = (dates: [Date | null, Date | null] | null) => {
+    if (dates) {
+      const [start, end] = dates;
+
+      if (startDate !== start) setEndDate(null);
+
+      handleSearchState('checkInDate', start);
+      handleSearchState('checkOutDate', end);
+      setStartDate(start);
+
+      if (end) {
+        setEndDate(end);
+        setCalendarState(false);
+      }
+    }
+  };
 
   return (
     <>
-      <input type="checkbox" id="calendar" className="modal-toggle" />
-      <div className="modal">
-        <div className="modal-box shadow-lg">
-          <div className="flex justify-center gap-3">
-            <div className="flex flex-col gap-2">
-              <span className="font-semibold">체크인</span>
-              <DatePicker
-                locale={ko}
-                selected={startDate}
-                onChange={onChange}
-                minDate={new Date(2023, 6, 1)}
-                maxDate={new Date(2023, 9, 1)}
-                startDate={startDate}
-                endDate={endDate}
-                selectsRange
-                inline
-                showDisabledMonthNavigation
-                closeOnScroll={true}
-              />
-            </div>
-          </div>
-          <div className="modal-action">
-            <label
-              htmlFor="calendar"
-              className="btn btn-sm bg-red-500 hover:bg-red-600 text-white"
-            >
-              확인
-            </label>
+      <div
+        className={`flex justify-center items-center absolute top-20 sm:top-9 left-0 sm:left-1/2 ${
+          calendarState ? 'block' : 'hidden'
+        }`}
+      >
+        <div className="flex gap-1 px-2 md:px-3 pt-3 rounded-lg bg-white shadow-lg z-10 opacity-1">
+          <div>
+            <DatePicker
+              locale={ko}
+              selected={startDate}
+              onChange={onCalendarChange}
+              minDate={new Date(2023, 6, 1)}
+              maxDate={new Date(2023, 9, 1)}
+              startDate={startDate}
+              endDate={endDate}
+              selectsRange
+              inline
+              showDisabledMonthNavigation
+              closeOnScroll={true}
+            />
           </div>
         </div>
-        <label className="modal-backdrop" htmlFor="calendar">
-          Close
-        </label>
       </div>
     </>
   );
