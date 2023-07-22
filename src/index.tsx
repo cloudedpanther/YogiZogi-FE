33fd83bb7aa3f88a28d9efa3c1ca5faa3e2658b9
cloudedpanther <rickeykoh11@gmail.com>
2023-07-23 01:45:02 +0900

    📝 chore: apply msw handler

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,14 @@ import 'tailwindcss/tailwind.css';
 import { RecoilRoot } from 'recoil';
 import React from 'react';
 import App from './App';
-// import { worker } from './mocks/worker';
+import { worker } from './mocks/worker';
 import KakaoMapScriptLoader from './components/map/KakaoMapScriptLoader';
 import Modal from './components/common/Modal';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-// if (process.env.NODE_ENV === 'development') {
-//   worker.start();
-// }
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
 
 const container = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(container);
